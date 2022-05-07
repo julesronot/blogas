b79dd846cd0de068ff7cdbed7605d488c132c257
Jules <jules.ronot@gmail.com>
2022-05-07 12:48:03 +0200

    insertion dans base nouveaux billets

--- src/controleur/BilletControleur.php
+++ src/controleur/BilletControleur.php
@@ -3,6 +3,7 @@
 namespace blogapp\controleur;
 
 use blogapp\modele\Billet;
+use blogapp\modele\Categorie;
 use blogapp\vue\BilletVue;
 use blogapp\vue\IndexVue;
 
@@ -38,6 +39,27 @@ class BilletControleur {
     public function ajoute($rq, $rs, $args) {
       $titre = filter_var($rq->getParsedBodyParam('titre'), FILTER_SANITIZE_STRING);
       $body = filter_var($rq->getParsedBodyParam('message'), FILTER_SANITIZE_STRING);
+      $nomcateg = filter_var($rq->getParsedBodyParam('categorie'), FILTER_SANITIZE_STRING);
+      $categ = Categorie::select('id')->where('titre', '=', $nomcateg)->first();
+      $idcateg = $categ->id;
+
+      if ($titre == NULL){
+        $this->cont->flash->addMessage('error', "Donnez un titre à votre billet.");
+        return $rs->withRedirect($this->cont->router->pathFor('billet_nouveau'));
+      }
+
+      if ($body == NULL){
+        $this->cont->flash->addMessage('error', "Vous n'avez rien à dire ?");
+        return $rs->withRedirect($this->cont->router->pathFor('billet_nouveau'));
+      }
+
+      $billet = new Billet() ;
+      $billet->titre = $titre ;
+      $billet->body = $body ;
+      $billet->cat_id = $idcateg ;
+      $billet->date = date("y-m-d");
+      $billet->save();
+
       $this->cont->flash->addMessage('info', "Billet $titre ajouté !");
       return $rs->withRedirect($this->cont->router->pathFor('billet_liste'));
     }
