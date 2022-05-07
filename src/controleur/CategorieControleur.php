91d61ab1fc411055615c34f0b741c17b34255277
Jules <jules.ronot@gmail.com>
2022-05-07 14:04:40 +0200

    Ajout de catégorie et insertion dans la base

--- src/controleur/CategorieControleur.php
+++ src/controleur/CategorieControleur.php
@@ -22,6 +22,31 @@ class CategorieControleur {
     public function ajoute($rq, $rs, $args) {
       $titre = filter_var($rq->getParsedBodyParam('titre'), FILTER_SANITIZE_STRING);
       $descr = filter_var($rq->getParsedBodyParam('description'), FILTER_SANITIZE_STRING);
+
+      $test_titre = Categorie::where('titre', '=', $titre)->first() ;
+
+      if ($titre == NULL){
+        $this->cont->flash->addMessage('error', "Donnez un titre à votre catégorie.");
+        return $rs->withRedirect($this->cont->router->pathFor('categ_nouveau'));
+      }
+
+      if ($descr == NULL){
+        $this->cont->flash->addMessage('error', "Donnez une description pour votre catégorie.");
+        return $rs->withRedirect($this->cont->router->pathFor('categ_nouveau'));
+      }
+
+      if ($test_titre != NULL){
+          $this->cont->flash->addMessage('error',
+                                  "Cette catégorie existe déjà.
+                                  Choisissez-lui un autre nom.");
+          return $rs->withRedirect($this->cont->router->pathFor('categ_nouveau'));
+      }
+
+      $categ = new Categorie() ;
+      $categ->titre = $titre ;
+      $categ->description = $descr ;
+      $categ->save() ;
+
       $this->cont->flash->addMessage('info', "Catégorie $titre ajoutée !");
       return $rs->withRedirect($this->cont->router->pathFor('billet_liste'));
     }
